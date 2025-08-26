b4a2aaff18fbd97e0f699f9f1fa2782b0a095cca
izlatovratskiy <izlatovratskiy@ozon.ru>
2025-08-26 18:30:47 +0300

    condition added

---
+++ solution/level_1/solution/main.go
@@ -0,0 +1,60 @@
+/*
+	Необходимо реализовать структуру MultiReader, которая объединяет
+	несколько объектов, реализующих интерфейс SizedReadSeekCloser.
+	Структура MultiReader должна сама реализовывать интерфейс SizedReadSeekCloser.
+
+	Операции, которые должен поддерживать MultiReader:
+
+	Read: должен читать данные последовательно из всех переданных ридеров в том
+			же порядке, в котором они переданы в NewMultiReader
+	Seek: должен позволять перемещать курсор на заданную позицию в объединенной
+			последовательности ридеров.
+	Close: должен закрыть все ридеры.
+	Size: должен возвращать суммарный размер данных всех ридеров.
+*/
+
+/*
+type io.ReadSeekCloser interface {
+    Read(p []byte) (n int, err error)
+    Seek(offset int64, whence int) (int64, error)
+    Close() error
+}
+*/
+
+package main
+
+import "io"
+
+type SizedReadSeekCloser interface {
+	io.ReadSeekCloser
+	Size() int64
+}
+
+type MultiReader struct {
+	// put your code here...
+}
+
+func (m MultiReader) Read(p []byte) (n int, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m MultiReader) Seek(offset int64, whence int) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m MultiReader) Close() error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m MultiReader) Size() int64 {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewMultiReader(rs ...SizedReadSeekCloser) *MultiReader {
+	// put your code here...
+	return nil
+}
